b196d5f695621af696b3d06702e4503d8aa72f6b
Przemysław Gierski <pgierski@o2.pl>
2016-02-18 12:33:09 +0100

    ol3 test1

--- ol3/map.js
+++ ol3/map.js
@@ -41,131 +41,17 @@ var editableLayer = function(workspace, layerName, WFSurl) {
         style: polygonStyleFunction,
         //maxResolution: 50
     });
-
-
 };
 
 var rury_gazociagu = new editableLayer('atrem', 'rury_gazociagu', 'http://uslugi.giap.pl/geoserver/wfs');
 map.addLayer(rury_gazociagu.vector);
 
+var stacje_gazowe = new editableLayer('atrem', 'stacje_gazowe', 'http://uslugi.giap.pl/geoserver/wfs');
+map.addLayer(stacje_gazowe.vector);
+
 console.log('PINGWIN: rury_gazociagu', rury_gazociagu);
 
 var editableLayers = {};
-
-var vectorSource2 = new ol.source.Vector({
-    format: new ol.format.GeoJSON(),
-    url: function (extent) {
-        return 'http://uslugi.giap.pl/geoserver/wfs?service=WFS&' +
-            'version=1.1.0&request=GetFeature&typename=atrem:stacje_gazowe&' +
-            'outputFormat=application/json&srsname=EPSG:3857&' +
-            'bbox=' + extent.join(',') + ',EPSG:3857';
-    },
-    strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
-        maxZoom: 19
-    }))
-});
-
-
-var getText = function(feature, resolution) {
-    var type = 'normal';
-    var maxResolution = 100;
-    var featureValue = feature.get('atrs1');
-    var text = '';
-    if (featureValue) {
-        text = featureValue;
-    }
-
-    if (resolution > maxResolution) {
-        text = '';
-    } else if (type == 'hide') {
-        text = '';
-    } else if (type == 'shorten') {
-        text = text.trunc(12);
-    } else if (type == 'wrap') {
-        text = stringDivider(text, 16, '\n');
-    }
-
-    return text;
-};
-
-var createTextStyle = function(feature, resolution) {
-    var align = 'center';
-    var baseline = 'middle';
-    var size = '10px';
-    var offsetX = 0;
-    var offsetY = 12;
-    var weight = 'bold';
-    var rotation = 0;
-    var fontName = 'Verdana';
-    var font = weight + ' ' + size + ' ' + fontName;
-    var fillColor = 'blue';
-    var outlineColor = '#ffffff';
-    var outlineWidth = 3;
-
-    return new ol.style.Text({
-        textAlign: align,
-        textBaseline: baseline,
-        font: font,
-        text: getText(feature, resolution),
-        fill: new ol.style.Fill({color: fillColor}),
-        stroke: new ol.style.Stroke({color: outlineColor, width: outlineWidth}),
-        offsetX: offsetX,
-        offsetY: offsetY,
-        rotation: rotation
-    });
-};
-
-function createStrokeStyle(feature, resolution) {
-    var featureValue = feature.get('srednica');
-    var diameter = 400;
-    if (featureValue) {
-        diameter = featureValue;
-    }
-
-    var strokeColor = 'rgba(255, 239, 8, 255)';
-    var strokeWidth = 3;
-    if (diameter < 150) {strokeWidth = 2; strokeColor = 'rgba(255, 133, 62, 255)'}
-    else if (diameter >= 150 && diameter < 350) strokeWidth = 3;
-    else if (diameter >= 350 && diameter < 500) strokeWidth = 4;
-    else if (diameter >= 500) strokeWidth = 5;
-
-    return new ol.style.Stroke({
-        color: strokeColor,
-        width: strokeWidth
-    });
-}
-
-function polygonStyleFunction(feature, resolution) {
-    return new ol.style.Style({
-        stroke: createStrokeStyle(feature, resolution),
-        fill: new ol.style.Fill({
-            color: 'rgba(0, 0, 255, 0.05)'
-        }),
-        image: new ol.style.Circle({
-            radius: 5,
-            fill: new ol.style.Fill({
-                color: '#0000ff'
-            })
-        }),
-        text: createTextStyle(feature, resolution)
-    });
-}
-
-/*var vector = new ol.layer.Vector({
-    source: vectorSource,
-    style: polygonStyleFunction,
-    //maxResolution: 50
-});
-
-map.addLayer(vector);*/
-
-var vector2 = new ol.layer.Vector({
-    source: vectorSource2,
-    style: polygonStyleFunction,
-    //maxResolution: 50
-});
-
-map.addLayer(vector2);
 
 
 
