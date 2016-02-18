60c5fa8d040681b8aa9d42c13f79408778e6fbd7
Przemysław Gierski <pgierski@o2.pl>
2016-02-18 17:00:45 +0100

    ol3 test1

--- ol3/ui.js
+++ ol3/ui.js
@@ -35,7 +35,7 @@ $('#customControlSave').on('click', function () {
 });
 
 $currentEditLayerChoice.on('change', function () {
-    console.log('PINGWIN: $interaction_type.val()', $interaction_type.val());
+    console.log('PINGWIN: $interaction_type.value', $interaction_type.value);
     if ($interaction_type.val() === 'draw') {
         if (this.value == 'rury_gazociagu') {
             addDrawInteraction($currentEditLayerChoice.val(), 'LineString');
