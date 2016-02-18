8dcb0f3f88a1e2b9e61e4a869e3b67fdb5867d32
Przemysław Gierski <pgierski@o2.pl>
2016-02-18 11:58:57 +0100

    ol3 test1

--- ol3/map.js
+++ ol3/map.js
@@ -278,19 +278,6 @@ function deleteFeatures() {
         window.alert("Wybierz najpierw obiekt do usunięcia");
 }
 
-proj4.defs("EPSG:2180", "+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
-
-var transformationFromWebToPL = ol.proj.getTransform('EPSG:3857', 'EPSG:2180');
-
-var transformationFlipCoords = function (a) {
-    for (var i = 0; i < a.length; i += 2) {
-        var t = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = t;
-    }
-    return a;
-};
-
 function modifiedFeatures(event) {
     console.log('PINGWIN: event', event);
     var modifiedFeatures = event.features.getArray();
@@ -340,25 +327,6 @@ var formatGML = new ol.format.GML({
     featureType: 'rury_gazociagu',
     srsName: 'EPSG:2180'
 });
-
-function removeLowerCaseGeometryNodeForInsert(node) {
-    var geometryNodes = node.getElementsByTagName("geometry"), element;
-    while (geometryNode = geometryNodes[0]) {
-        geometryNode.parentNode.removeChild(geometryNode);
-    }
-}
-
-function removeNodeForWfsUpdate(node, valueToRemove) {
-    var propNodes = node.getElementsByTagName("Property");
-    for (var i = 0; i < propNodes.length; i++) {
-        var propNode = propNodes[i];
-        var propNameNode = propNode.firstElementChild;
-        var propNameNodeValue = propNameNode.firstChild;
-        if (propNameNodeValue.nodeValue === valueToRemove) {
-            propNode.parentNode.removeChild(propNode);
-        }
-    }
-}
 
 var transactWFS = function () {
 
