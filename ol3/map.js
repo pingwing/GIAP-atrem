91a9a41e5ad399610cecfecfdd9f9977c2b8f29d
Przemysław Gierski <pgierski@o2.pl>
2016-02-18 12:04:30 +0100

    ol3 test1

--- ol3/map.js
+++ ol3/map.js
@@ -321,46 +321,6 @@ function uid() {
     return id;
 }
 
-var formatWFS = new ol.format.WFS();
-var formatGML = new ol.format.GML({
-    featureNS: 'atrem',
-    featureType: 'rury_gazociagu',
-    srsName: 'EPSG:2180'
-});
-
-var transactWFS = function () {
-
-    var featuresToUpdate = [];
-    _.each(_.values(featuresToUpdateObject), function(modifiedFeat) {
-        var geometryInMapCRS = modifiedFeat.getGeometry();
-        var geometryInMapCRSClone = geometryInMapCRS.clone();
-        geometryInMapCRSClone.applyTransform(transformationFromWebToPL);
-        geometryInMapCRSClone.applyTransform(transformationFlipCoords);
-        modifiedFeat.set('geom', geometryInMapCRSClone);
-        featuresToUpdate.push(modifiedFeat);
-    });
-
-    console.log('PINGWIN: w transactWFS');
-    console.log('PINGWIN: featuresToInsert', featuresToInsert);
-    console.log('PINGWIN: featuresToUpdate', featuresToUpdate);
-    console.log('PINGWIN: featuresToDelete', featuresToDelete);
-
-    var node = formatWFS.writeTransaction(featuresToInsert, featuresToUpdate, featuresToDelete, formatGML);
-
-    removeLowerCaseGeometryNodeForInsert(node);
-    removeNodeForWfsUpdate(node, "geometry");
-
-    var s = new XMLSerializer();
-    var str = s.serializeToString(node);
-    $.ajax('http://uslugi.giap.pl/geoserver/wfs', {
-        type: 'POST',
-        dataType: 'xml',
-        processData: false,
-        contentType: 'text/xml',
-        data: str
-    }).done();
-};
-
 /*var deleteFeature = function () {
  selectInteraction.getFeatures().on('change:length', function (e) {
  transactWFS('delete', e.target.item(0));
---
+++ ol3/writeWFS.js
@@ -0,0 +1,3 @@
+/**
+ * Created by przemek on 18.02.2016.
+ */
