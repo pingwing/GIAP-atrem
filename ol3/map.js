c5ec5d22fb92eb9a8b9837444ba556c7fc6e957b
Przemysław Gierski <pgierski@o2.pl>
2016-02-18 12:40:12 +0100

    ol3 test1

--- ol3/map.js
+++ ol3/map.js
@@ -23,18 +23,19 @@ var map = new ol.Map({
     ])
 });
 
-var editableLayer = function(workspace, layerName, WFSurl) {
+var editableLayer = function (workspace, layerName, WFSurl) {
     this.vectorSource = new ol.source.Vector({
         format: new ol.format.GeoJSON(),
         url: function (extent) {
-            return WFSurl+'?service=WFS&' +
-                'version=1.1.0&request=GetFeature&typename='+workspace+':'+layerName+'&' +
+            return WFSurl + '?service=WFS&' +
+                'version=1.1.0&request=GetFeature&typename=' + workspace + ':' + layerName + '&' +
                 'outputFormat=application/json&srsname=EPSG:3857&' +
                 'bbox=' + extent.join(',') + ',EPSG:3857';
         },
         strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
             maxZoom: 19
-        }))});
+        }))
+    });
 
     this.vector = new ol.layer.Vector({
         source: this.vectorSource,
@@ -44,16 +45,13 @@ var editableLayer = function(workspace, layerName, WFSurl) {
 };
 
 var rury_gazociagu = new editableLayer('atrem', 'rury_gazociagu', 'http://uslugi.giap.pl/geoserver/wfs');
-map.addLayer(rury_gazociagu.vector);
-
 var stacje_gazowe = new editableLayer('atrem', 'stacje_gazowe', 'http://uslugi.giap.pl/geoserver/wfs');
-map.addLayer(stacje_gazowe.vector);
 
-console.log('PINGWIN: rury_gazociagu', rury_gazociagu);
+var editableLayers = [rury_gazociagu, stacje_gazowe];
 
-var editableLayers = {};
-
-
+_.each(editableLayers, function (layer) {
+    map.addLayer(layer.vector);
+});
 
 /*var deleteFeature = function () {
  selectInteraction.getFeatures().on('change:length', function (e) {
