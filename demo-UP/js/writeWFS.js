c67b88e31aa090746f870d8925e95520484c0b18
Przemysław Gierski <pgierski@o2.pl>
2016-03-03 20:02:55 +0100

    demo2 update

--- demo-UP/js/writeWFS.js
+++ demo-UP/js/writeWFS.js
@@ -26,12 +26,22 @@ var transactWFS = function () {
             _thisLayerFeaturesToUpdate.push(modifiedFeat);
         });
 
+        var _thisLayerFeaturesToInsertCRS = [];
+        _.each(_thisLayerFeaturesToInsert, function (insertFeat) {
+            var geometryInMapCRS = insertFeat.getGeometry();
+            var geometryInMapCRSClone = geometryInMapCRS.clone();
+            geometryInMapCRSClone.applyTransform(transformationFromWebToPL);
+            geometryInMapCRSClone.applyTransform(transformationFlipCoords);
+            insertFeat.set('geom', geometryInMapCRSClone);
+            _thisLayerFeaturesToUpdate.push(insertFeat);
+        });
+
         var _totalFeaturesInTransaction = _thisLayerFeaturesToInsert.length + _thisLayerFeaturesToUpdate.length + _thisLayerFeaturesToDelete.length;
 
         /*console.log('PINGWIN: _thisLayerFeaturesToInsert', _thisLayerFeaturesToInsert);
         console.log('PINGWIN: _thisLayerFeaturesToUpdate', _thisLayerFeaturesToUpdate);
         console.log('PINGWIN: _thisLayerFeaturesToDelete', _thisLayerFeaturesToDelete);*/
-        var node = formatWFS.writeTransaction(_thisLayerFeaturesToInsert, _thisLayerFeaturesToUpdate, _thisLayerFeaturesToDelete, formatGML);
+        var node = formatWFS.writeTransaction(_thisLayerFeaturesToInsertCRS, _thisLayerFeaturesToUpdate, _thisLayerFeaturesToDelete, formatGML);
 
         if (_totalFeaturesInTransaction > 0) {
             //console.log('PINGWIN: w transactWFS dla warstwy',layer.layerName, 'zmienia',_totalFeaturesInTransaction,'rekordów');
